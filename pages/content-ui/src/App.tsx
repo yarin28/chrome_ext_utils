00f5f63c2a33bcd9ffba731992f7346a389bf2fc
Yarin Greenfeld <yarin.greenfeld@gmail.com>
2025-02-05 08:09:46 +0200

    added grid example

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from '@extension/ui';
 import { Label } from '@extension/ui';
 import { Input } from '@extension/ui';
 import { useStorage } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
+import { AgGridReact } from 'ag-grid-react';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import {
   Dialog,
   DialogContent,
@@ -18,10 +20,34 @@ import {
 export default function App() {
   const theme = useStorage(exampleThemeStorage);
 
+  ModuleRegistry.registerModules([AllCommunityModule]);
   useEffect(() => {
     console.log('content ui loaded');
   }, []);
+  const GridExample = () => {
+    // Row Data: The data to be displayed.
+    const [rowData, setRowData] = useState([
+      { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+      { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+      { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+    ]);
 
+    // Column Definitions: Defines the columns to be displayed.
+    const [colDefs, setColDefs] = useState([
+      { field: 'make' },
+      { field: 'model' },
+      { field: 'price' },
+      { field: 'electric' },
+    ]);
+
+    return (
+      <div
+        // define a height because the Data Grid will fill the size of the parent container
+        style={{ height: 500, width: '100%' }}>
+        <AgGridReact rowData={rowData} columnDefs={colDefs} />
+      </div>
+    );
+  };
   return (
     <div className="flex items-center justify-between gap-2 rounded bg-green-100 px-2 py-1">
       <div className="flex gap-1 text-green-500">
@@ -41,20 +67,7 @@ export default function App() {
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>Make changes to your profile here. Click save when you're done.</DialogDescription>
           </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input id="name" value="Pedro Duarte" className="col-span-3" />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" className="text-right">
-                Username
-              </Label>
-              <Input id="username" value="@peduarte" className="col-span-3" />
-            </div>
-          </div>
+          <GridExample></GridExample>
           <DialogFooter>
             <Button type="submit">Save changes</Button>
           </DialogFooter>
