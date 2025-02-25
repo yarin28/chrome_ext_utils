3546c168eccfe1daf8de734e2bfd76eafa10bd56
Yarin Greenfeld <yarin.greenfeld@gmail.com>
2025-02-25 23:06:25 +0200

    removed the settings object from app to options

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -35,21 +35,6 @@ export default function App() {
   const [formData, setFormData] = useState<FormData | null>(null);
   const observerRef = useRef<MutationObserver | null>(null);
 
-  //TODO: move the settings object to some storage place
-  const SETTINGS = {
-    TABLE_URLS: {
-      qas: 'https://www.w3schools.com/html/html_tables.asp',
-      preprod: 'https://www.w3schools.com/html/html_tables.asp',
-    },
-    TABLE_ID: 'customers',
-    TITLE_COLORS: {
-      qas: '#00FF00',
-      preprod: '#bd24eb',
-      prod: '#0000FF',
-    },
-    STORAGE_KEY: 'loginCredentials',
-  };
-
   const fillFromWithCredentials = (credentials: Credentials) => {
     if (!formData) {
       toast.error('could not find the form on the website');
--- pages/options/src/defaultSettings.tsx
+++ pages/options/src/defaultSettings.tsx
@@ -1,5 +1,13 @@
 export const defaultSettings = {
-  theme: 'light',
-  featureFlag: true,
-  customOption: 'value',
+  TABLE_URLS: {
+    qas: 'https://www.w3schools.com/html/html_tables.asp',
+    preprod: 'https://www.w3schools.com/html/html_tables.asp',
+  },
+  TABLE_ID: 'customers',
+  TITLE_COLORS: {
+    qas: '#00FF00',
+    preprod: '#bd24eb',
+    prod: '#0000FF',
+  },
+  STORAGE_KEY: 'loginCredentials',
 };
